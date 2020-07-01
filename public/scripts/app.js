46f95b6cbd249011e6ffd07c5ceb116008ab99c9
petros <pit.trak@gmail.com>
2020-07-01 20:07:34 +0300

    translate jsx into js code with babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,13 @@
 console.log('App.js is running!');
 
-var template = <p>This is a JSX from app.js!</p>
+//var template = <p>This is a JSX from app.js!</p>
 var appRoot = document.getElementById('app');
+
+var template = React.createElement(
+    "h1",
+    {id: "someid"},
+    "Something new!"
+);
 
 // render gets two parameters. 1st is 'what' and 2nd is 'where'
 // What do you want to render and
