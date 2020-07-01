9283918a16aafade3121426a357c5be7eeb12e0f
petros <pit.trak@gmail.com>
2020-07-01 20:04:17 +0300

    intro to JSX

---
+++ public/scripts/app.js
@@ -0,0 +1,9 @@
+console.log('App.js is running!');
+
+var template = <p>This is a JSX from app.js!</p>
+var appRoot = document.getElementById('app');
+
+// render gets two parameters. 1st is 'what' and 2nd is 'where'
+// What do you want to render and
+// Where do you want to render it
+ReactDOM.render(template, appRoot);
