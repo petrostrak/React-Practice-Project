81dee04667084bb082f68b7939fa8b31307f1ec5
petros <pit.trak@gmail.com>
2020-07-02 11:33:52 +0300

    JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,56 +2,74 @@
 
 console.log('App.js is running!');
 
-// JSX - JavaScript XML
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Indecision App'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'This is some info'
-  ),
-  React.createElement(
-    'ol',
+var appObject = {
+    title: 'React Practice Project',
+    subtitle: 'I hate js!'
+
+    // JSX - JavaScript XML
+};var template = React.createElement(
+    'div',
     null,
     React.createElement(
-      'li',
-      null,
-      'Item one'
+        'h1',
+        null,
+        appObject.title
     ),
     React.createElement(
-      'li',
-      null,
-      'Item two'
+        'p',
+        null,
+        appObject.subtitle
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
     )
-  )
 );
 
+var userName = 'Petros Trakadas';
+var userAge = 33;
+var userLocation = 'Alimos';
+
+var user = {
+    firstName: 'Petros',
+    lastName: 'Trakadas',
+    age: 33,
+    location: 'Athens, Greece'
+};
 var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
+    'div',
     null,
-    'Petros Trak'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'Age: 34'
-  ),
-  React.createElement(
-    'p',
-    null,
-    'Location: Athens, Greece'
-  )
+    React.createElement(
+        'h1',
+        null,
+        user.firstName,
+        ', ',
+        user.lastName
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: ',
+        user.location
+    )
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 console.log('App.js is running!');
 
+var appObject = {
+    title: 'React Practice Project',
+    subtitle: 'I hate js!'
+}
+
 // JSX - JavaScript XML
 var template = (
   <div>
-    <h1>Indecision App</h1>
-    <p>This is some info</p>
+    <h1>{appObject.title}</h1>
+    <p>{appObject.subtitle}</p>
     <ol>
       <li>Item one</li>
       <li>Item two</li>
@@ -12,14 +17,24 @@ var template = (
   </div>
 );
 
+var userName = 'Petros Trakadas'
+var userAge = 33
+var userLocation = 'Alimos'
+
+var user = {
+    firstName: 'Petros',
+    lastName: 'Trakadas',
+    age: 33,
+    location: 'Athens, Greece'
+}
 var templateTwo = (
     <div>
-        <h1>Petros Trak</h1>
-        <p>Age: 34</p>
-        <p>Location: Athens, Greece</p>
+        <h1>{user.firstName}, {user.lastName}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
     </div>
 );
 
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
