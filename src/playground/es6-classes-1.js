a1b32c6c5504dfcfbeeab8301a93e2195387c098
petros <pit.trak@gmail.com>
2020-07-03 11:22:21 +0300

    class hierarchy in jsx

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -13,7 +13,47 @@ class Person{
     }
 }
 
-const me = new Person('Petros', 34);
-console.log(me.getGreeting())
-const other = new Person();
+class Student extends Person{
+    constructor(name, age, major = 'undecided'){
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor(){
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+        if(this.major){
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person{
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    getGreeting(){
+        let isHomeLocation = super.getGreeting();
+        if(this.homeLocation){
+            isHomeLocation += ` I'm visiting from Athens, Greece!`
+        }
+        return isHomeLocation;
+
+    }
+}
+
+const me = new Student('Petros', 34, 'Computer Science');
+console.log(me.hasMajor())
+console.log(me.getDescription())
+
+const other = new Student();
 console.log(other)
+
+const petros = new Traveler('Petran', 34, 'Athens');
+console.log(petros.getGreeting())
