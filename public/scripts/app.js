12a0436cfe8845ce918094f5c378748d3934ab01
petros <pit.trak@gmail.com>
2020-07-03 17:04:58 +0300

    events and methods in components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -81,6 +81,11 @@ var Action = function (_React$Component3) {
   }
 
   _createClass(Action, [{
+    key: 'handlePick',
+    value: function handlePick() {
+      alert('handlePick');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
@@ -88,7 +93,7 @@ var Action = function (_React$Component3) {
         null,
         React.createElement(
           'button',
-          null,
+          { onClick: this.handlePick },
           'What should I do?'
         )
       );
@@ -108,11 +113,21 @@ var Options = function (_React$Component4) {
   }
 
   _createClass(Options, [{
+    key: 'handleRemoveAll',
+    value: function handleRemoveAll() {
+      alert('handleRemoveAll');
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
         'div',
         null,
+        React.createElement(
+          'button',
+          { onClick: this.handleRemoveAll },
+          'Remove all'
+        ),
         React.createElement(
           'ol',
           null,
@@ -168,6 +183,15 @@ var AddOption = function (_React$Component6) {
   }
 
   _createClass(AddOption, [{
+    key: 'handleAddOption',
+    value: function handleAddOption(e) {
+      e.preventDefault();
+      var option = e.target.elements.option.value.trim();
+      if (option) {
+        alert(option);
+      }
+    }
+  }, {
     key: 'render',
     value: function render() {
       return React.createElement(
@@ -175,12 +199,12 @@ var AddOption = function (_React$Component6) {
         null,
         React.createElement(
           'form',
-          null,
-          React.createElement('input', { type: 'text' }),
+          { onSubmit: this.handleAddOption },
+          React.createElement('input', { type: 'text', name: 'option' }),
           React.createElement(
             'button',
             null,
-            'Submit Option'
+            'Add Option'
           )
         )
       );
--- src/app.js
+++ src/app.js
@@ -28,19 +28,30 @@ class Header extends React.Component{
 }
 
 class Action extends React.Component{
+
+  handlePick(){
+    alert('handlePick')
+  }
+
   render(){
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component{
+
+  handleRemoveAll(){
+    alert('handleRemoveAll')
+  }
+
   render(){
     return (
       <div>
+      <button onClick={this.handleRemoveAll}>Remove all</button>
         <ol>
           {this.props.options.map((option)=>{return <Option key={option} optionText={option}/>})}
         </ol>
@@ -62,12 +73,21 @@ class Option extends React.Component{
 }
 
 class AddOption extends React.Component{
+  
+  handleAddOption(e){
+    e.preventDefault();
+    const option = e.target.elements.option.value.trim();
+    if(option){
+      alert(option)
+    }
+  }
+
   render(){
     return (
       <div>
-        <form>
-          <input type="text"/>
-          <button>Submit Option</button>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
         </form>
       </div>
     );
