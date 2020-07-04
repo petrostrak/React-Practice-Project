38d2f99d146aee73224c8d8ab903b3e1987674bf
petros <pit.trak@gmail.com>
2020-07-04 21:40:36 +0300

    added defaultProp values

--- src/playground/counter-app.js
+++ src/playground/counter-app.js
@@ -1,51 +1,48 @@
-
-var userName = 'Petros Trakadas'
-var userAge = 33
-var userLocation = 'Alimos'
-
-// function getLocation(location){
-//     if(location){
-//         return <p>{location}</p>;
-//     }
-// }
-
-const getLocation = (location) =>{
-    if(location) return <p>{location}</p>;
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: props.count
+    };
+  }
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1
+      };
+    });
+  }
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      };
+    });
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0
+      };
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
 }
 
-var user = {
-    firstName: 'Petros',
-    lastName: 'Trakadas',
-    age: 33,
-    location: 'Athens, Greece'
-}
-var templateTwo = (
-    <div>
-        <h1>{user.firstName ? user.firstName : 'Unknown first name'}, {user.lastName ? user.lastName : 'Unknown last name'}</h1>
-        {user.age >= 18 && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
-
-let count = 0;
-
-const addOne = ()=>{count++;console.log('addOne',count);renderCounterApp();};
-const minusOne = ()=>{count--;console.log('minusOne',count);renderCounterApp();};
-const resetCounter = ()=>{count =0;console.log('reseted',count);renderCounterApp();};
-
-var appRoot = document.getElementById('app');
-
-const renderCounterApp = () =>{
-  const templateTree = (
-    <div>
-      <h1>Count : {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={resetCounter}>Reset</button>
-    </div>
-  );
-  
-  ReactDOM.render(templateTree, appRoot);
+Counter.defaultProps = {
+  count : 0
 }
 
-renderCounterApp();
+ReactDOM.render(<Counter />, document.getElementById('app'));
