033d3157933e4a70b6926cb39520c41cb08812b8
petros <pit.trak@gmail.com>
2020-07-05 20:38:41 +0300

    ES6 import && export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-console.log('app.js is running!!!')
+//import './utils.js'
+import {square, add} from './utils'; 
+import {isAdult, canDrink} from './person';
+
+console.log('app.js is running!!!');
+console.log(square(4));
+console.log(add(3, 7));
+console.log(isAdult(16));
+console.log(canDrink(21));
